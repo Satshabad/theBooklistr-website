e7946d7f09be86291851d75e598059ca11739d89
Satshabad Khalsa <satshabad.music@gmail.com>
2012-05-08 18:17:26 -0700

    changed to serev on gunicorn instead of runserver

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SERVER_DIR, 'static')
+STATIC_ROOT = os.path.join(SERVER_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,6 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SERVER_DIR, 'app/static/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -81,7 +82,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-	os.path.join(SERVER_DIR, 'app/static/'),
 )
 
 # Make this unique, and don't share it with anybody.
@@ -123,7 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'gunicorn'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
