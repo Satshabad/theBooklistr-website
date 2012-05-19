633e7115a554ce103c3ebf0471340c28590cdf1e
Satshabad Khalsa <satshabad.music@gmail.com>
2012-05-19 13:22:38 -0700

    implemented error checking for sell form

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from forms import SellBookForm
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseRedirect
 
-
-
+# include this decorator on all post request view functions
+@csrf_protect
 def sell(request):
     if request.method == 'POST':
         form = SellBookForm(request.POST)
@@ -13,13 +15,15 @@ def sell(request):
         # DO STUFF ###
           
           # Redirect to a confirmation of Book posting page 
-        return HttpResponseRedirect('/some/url')
-      
-    else:
-        form = SellBookForm()
+            return HttpResponseRedirect('/thanks.html')
+        else:
+            print 'here'
+            return render_to_response('sell.html', RequestContext(request,  {'form':form}))
+    form = SellBookForm()
 
-    return render_to_response('sell.html', {'form':form})
+    return render_to_response('sell.html', RequestContext(request,  {'form':form}))
 
+@csrf_protect
 def buy(request):
     if request.method == 'GET':
         pass    
@@ -40,6 +44,7 @@ def contact(request):
     c = RequestContext(request)
     return render_to_response("contact.html", c)
 
+@csrf_protect
 def about(request):
     c = RequestContext(request)
     return render_to_response("about.html", c)
