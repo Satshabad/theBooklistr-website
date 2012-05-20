5e54ceebc7addb9ef077e77f423f867a71b26e05
Ryan Montoya <rcmontoy@gmail.com>
2012-05-19 22:48:41 -0700

    Added settings for DB

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -14,12 +14,13 @@ SERVER_DIR = os.path.abspath(".")
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        #'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'hackathon', # Or path to database file if using sqlite3.
+        'USER' : 'root', # Not used with sqlite3.
+        'PASSWORD' : 'root', # Not used with sqlite3.
+        'HOST' : 'localhost', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT' : '3306', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
