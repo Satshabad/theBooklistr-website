e806525b8eebbbd8f850a9e640d9c71349d2a39a
Allen Eubank <adeubank@gmail.com>
2012-05-20 20:42:03 -0700

    Created drop down box for sell.html. Made changes to forms.py and changed the ListedBook Class to have the condition field have a max length of 5.

--- app/models.py
+++ app/models.py
@@ -26,7 +26,7 @@ class ListedBook(models.Model):
     isbn = models.CharField(max_length=30)
     email = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=4)
-    condition = models.CharField(max_length=50)
+    condition = models.CharField(max_length=5)
 
 class Book(models.Model):
     isbn = models.CharField(max_length=30)
