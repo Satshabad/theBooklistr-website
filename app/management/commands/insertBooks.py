d8489003be984cdaf9fbb2338dec7500bd72727f
Ryan Montoya <rcmontoy@gmail.com>
2012-05-20 17:02:26 -0700

    Added an insertBooks.py script that inserts books from scraped data into the DB

---
+++ app/management/commands/insertBooks.py
@@ -0,0 +1,46 @@
+# This script will insert all the courses from courseData.txt 
+# into the app_course table.
+
+from optparse import make_option
+from django.core.management.base import NoArgsCommand
+import pickle
+from app.models import Book
+
+class Command(NoArgsCommand):
+
+    help = "Whatever you want to print here"
+
+    option_list = NoArgsCommand.option_list + (
+        make_option('--verbose', action='store_true'),
+    )
+
+    def handle_noargs(self, **options):
+        data = pickle.load(open('courseData.txt'))
+        option_list = NoArgsCommand.option_list + (make_option('--verbose', action='store_true'),)
+
+        for dept in data:
+            for courses in dept['courses']:
+                newCourseName = courses['name']
+
+                for section in courses['sections']:
+                    newSectionName = section['name']
+
+                    for book in section['books']:
+                        newISBN = book['ISBN']
+                        newAuthor = book['author']
+                        newBinding = book['binding']
+                        newPrice = book['broncoListPrice']
+                        newEdition = book['edition']
+                        newRequired = book['isRequired']
+                        newBook = Book(
+                            isbn = newISBN,
+                            author = newAuthor,
+                            required = newRequired,
+                            broncoPrice = newPrice,
+                            binding = newBinding,
+                            edition = newEdition,
+                            courseName = newCourseName,
+                            sectionName = newSectionName
+                        )
+                        newBook.save()
+                        print newBook
--- app/models.py
+++ app/models.py
@@ -2,32 +2,26 @@ from django.db import models
 
 # Create your models here# Create your models here.
 class Course(models.Model):
-	name = models.CharField(max_length=30) # ex. CS 241.
-	courseID = models.IntegerField()
-	sectionID = models.CharField(max_length=30, primary_key=True)
-	sectionName = models.CharField(max_length=20) # part of PK
-	instructor = models.CharField(max_length=50)
+    name = models.CharField(max_length=30) # ex. CS 241.
+    courseID = models.IntegerField()
+    sectionID = models.CharField(max_length=30, primary_key=True)
+    sectionName = models.CharField(max_length=20) # part of PK
+    instructor = models.CharField(max_length=50)
 
-	#def __unicode__(self):
-	#	return u'%s %s %s %s %s' % (self.name, self.courseID, self.sectionID, self.sectionName, self.instructor)
-	
-#class Section(models.Model):
-#	courseName = models.CharField(max_length=30) # part of PK
-#	sectionName = models.CharField(max_length=20) # part of PK
-#	instructor = models.CharField(max_length=50)
-#	sectionID = models.CharField(max_length=30, primary_key=True)		# does this have use?
-	
 class Book(models.Model):
-	isbn = models.CharField(max_length=30, primary_key=True)
-	author = models.CharField(max_length=100)
-	required = models.CharField(max_length=10)
-	price = models.DecimalField(max_digits=10, decimal_places=2)
-	binding = models.CharField(max_length=15, null=True)
-	edition = models.CharField(max_length=4, null=True)
+    isbn = models.CharField(max_length=30,)
+    author = models.CharField(max_length=100)
+    required = models.CharField(max_length=50, null=True)
+    broncoPrice = models.DecimalField(max_digits=10, decimal_places=2)
+    binding = models.CharField(max_length=15, null=True)
+    edition = models.CharField(max_length=4, null=True)
+    # going to be looking for book via courseName and sectionName
+    courseName = models.CharField(max_length=30)
+    sectionName = models.CharField(max_length=30)
 
 class ListedBook(models.Model):
-	private_id = models.IntegerField(primary_key=True)
-	isbn = models.CharField(max_length=30)
-	email = models.CharField(max_length=100)
-	price = models.DecimalField(max_digits=10, decimal_places=4)
-	condition = models.CharField(max_length=50)
+    private_id = models.IntegerField(primary_key=True)
+    isbn = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=4)
+    condition = models.CharField(max_length=50)
