993fbc5906dca17d135239c096f1f71f863c7c00
Satshabad Khalsa <satshabad.music@gmail.com>
2012-05-22 10:56:14 -0700

    added local settings inclusion

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -160,9 +160,4 @@ LOGGING = {
         },
     }
 }
-import os
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+from local_settings import *
