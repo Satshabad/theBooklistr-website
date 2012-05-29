11aef35c9ec1409a52fe95fa148014f24af1a75c
Allen Eubank <adeubank@gmail.com>
2012-05-29 15:31:18 -0700

    Added fail message to views.py when a user search is in an irregular format. Added a section for a message to be displayed after a user conducts a search, and took out the custom css out of searchbase.html. Put custom css into  it's own file.

--- app/views.py
+++ app/views.py
@@ -134,7 +134,10 @@ def search(request):
             if re.match(r'^\w{1,5}\s*\d{3}$', request.GET['q']) is None \
                 or re.match(r'^\d{1,30}$', request.GET['s']) is None:
                 # The user has submitted an irregular section id or course name
-                return render_to_response('search.html')
+                message = "Sorry, we couldn't find what you were looking for."
+
+                c = RequestContext(request, {'message':message})
+                return render_to_response('search.html', c)
 
             books2 = Book.objects.filter(sectionID=request.GET['s'])
 
@@ -202,7 +205,10 @@ def search(request):
             # VALIDATE
             if re.match(r'^\w{1,5}\s*\d{3}$', request.GET['q']) is None:
                 # The user has submitted an irregular course name
-                return render_to_response('search.html')
+                message = "Sorry, we couldn't find what you were looking for."
+
+                c = RequestContext(request, {'message':message})
+                return render_to_response('search.html', c)
 
             # query the database for the courses with the name requested in q
             sections = Section.objects.filter(courseName = request.GET['q'])
