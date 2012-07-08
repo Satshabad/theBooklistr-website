c28608f5071fedfeab418d1427030d66752cf3aa
Satshabad Khalsa <satshabad.music@gmail.com>
2012-07-07 23:20:20 -0700

    added timedate field

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# course should be section
 class Section(models.Model):
     courseName = models.CharField(max_length=30) # ex. CS 241.
     sectionID = models.CharField(max_length=30, primary_key=True)
@@ -8,29 +7,14 @@ class Section(models.Model):
     instructor = models.CharField(max_length=150)
     quarterName = models.CharField(max_length=150)
 
-#class Book(models.Model):
-#    going to be looking for book via courseName and sectionName
-#    sectionID = models.CharField(max_length=30, primary_key=True)
-#    courseName = models.CharField(max_length=30)
-#    sectionName = models.CharField(max_length=30)
 
-#class ListedBook(models.Model):
-#    private_id = models.IntegerField(primary_key=True)
-#    isbn = models.CharField(max_length=30)
-#    email = models.CharField(max_length=100)
-#    price = models.DecimalField(max_digits=10, decimal_places=4)
-#    condition = models.CharField(max_length=50)
-
-# ideas
 class ListedBook(models.Model):
     secret_key = models.CharField(max_length=32)
     isbn = models.CharField(max_length=30)
     email = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     condition = models.CharField(max_length=5)
-    
-    def __unicode__(self):
-        return u"%s" % self.email
+    timeListed = models.DateTimeField(auto_now_add=True)
 
 class Book(models.Model):
     isbn = models.CharField(max_length=30)
